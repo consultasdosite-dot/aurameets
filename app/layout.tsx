6d910d0a6ef0bb2ea5e86b5cd80dab7ff42ed272
Oscar Ahumada <assessoria3@gmail.com>
2026-08-19 23:22:42 -0300

    Ativa consentimento de cookies no layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import CookieConsent from "@/components/CookieConsent";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
         className="flex min-h-full flex-col"
       >
         {children}
+        <CookieConsent />
       </body>
     </html>
   );
